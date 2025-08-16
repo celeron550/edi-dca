503f25a912bca2be1e1d111427cc2b2b58563337
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-08-16 16:04:30 -0300

    Implement AddOnIndex, Remove, Pop, and Set methods

--- ArrayList.go
+++ ArrayList.go
@@ -8,9 +8,11 @@ import(
 type List interface {
 	Size() int
 	Get(index int) (int,error)
+	Set(e int, index int)
 	Add(e int)
 	AddOnIndex(e int, index int) error
 	Remove(index int) error
+	Pop() error
 
 
 }
@@ -46,6 +48,26 @@ func (list *ArrayList) Add(e int) {
 	list.inserted++
 }
 
+func (list *ArrayList) AddOnIndex(e, index int) error {
+	if index >=0 && index <= list.inserted {
+		if list.inserted == len(list.v) {
+			list.doubleV()
+		}
+		for i := list.inserted; i > index; i-- {
+			list.v[i] = list.v[i-1]
+		}
+		list.v[index] = e
+		list.inserted++ 
+		return nil
+	} else {
+		if index == 0 {
+			return errors.New("Nao eh possivel adicionar no arraylist com indice < 0")
+		} else {
+			return errors.New("Nao eh possivel adicionar no arraylist em uma posição maior > arraylist.size")
+		}
+	}
+}
+
 func (list *ArrayList) doubleV(){
 
 	newV := make([]int, list.inserted  *2)
@@ -55,6 +77,43 @@ func (list *ArrayList) doubleV(){
 		
 	}
 	list.v = newV
+}
+
+func (list *ArrayList) Remove(index int) error {
+	if index >=0 && index < list.inserted {
+		for i := index; i < list.inserted-1; i++ {
+			list.v[i] = list.v[i+1]			
+		}
+		list.inserted--
+		return nil
+	} else {
+		if index < 0 {
+			return errors.New("Nao eh possivel remover no arraylist com indice < 0")
+		} else {
+			return errors.New("Nao eh possivel remover no arraylist com indice > arraylist.size")
+		}
+	}
+}
+
+func (list *ArrayList) Pop() error {
+	if list.inserted == 0 {
+		return errors.New("ArrayList vazio")
+	}
+	list.inserted--
+	return nil
+}
+
+func (list *ArrayList) Set(e int, index int) error {
+	if index >= 0 && index < list.inserted {
+		list.v[index] = e
+		return nil
+	} else {
+		if index < 0 {
+			return errors.New("Nao eh possivel substituir no arraylist com indice < 0")
+		} else {
+			return errors.New("Nao eh possivel substituir no arraylist com indice > arraylist.size")
+		}
+	}
 }
 
 func main() {
@@ -69,6 +128,23 @@ func main() {
 	
 	val, _ = l.Get(49)
 	fmt.Println("Valor na posicao 49: ",val)
+
+	_ = l.AddOnIndex(51,50)
+	val, _ = l.Get(50)
+	fmt.Println("valor na posicao 50: ",val)
+
+	_ = l.Remove(50)
+	val, _ = l.Get(49)
+	fmt.Println("valor na posicao 49: ",val)
+
+	_ = l.Pop()
+	val, _ = l.Get(48)
+	fmt.Println("valor na posicao 48: ",val)
+
+	_ = l.Set(666,48)
+	val, _ = l.Get(48)
+	fmt.Println("valor na posicao 48 apos a troca: ",val)
+	
 
 
 }
