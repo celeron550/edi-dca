ac6dae6d7ecb1887df28f68d3b161a83e8998d7d
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-08-21 15:56:52 -0300

    por mim ja deu dps conserto o dll

--- list/DoublyLinkedlist.go
+++ list/DoublyLinkedlist.go
@@ -46,5 +46,55 @@ func (list *DoublyLinkedList) Add(e int) {
 		next:     nil,
 		previous: nil,
 	}
+	
+	if list.head == nil {
+		list.head = Novo
+		list.inserted ++
+		return
+	}
+	
+	aux := list.head
 
+	for aux.next != nil {
+		aux = aux.next
+	}
+
+	aux.next = Novo
+	Novo.previous = aux
+	list.inserted++
+	
+
+}
+
+func (list *DoublyLinkedList) AddOnIndex(e, index int) error {
+	if index >= 0 && index <= list.inserted {
+		Novo := &Node2P{
+			val: e,
+			next: nil,
+			previous: nil,
+		}
+		if index == 0 {
+			// novo.previous fica nil, next aponta pro antigo "0" 
+			// e o head aponta pro novo Node2P (ou usa o Add())
+			Novo.next = list.head
+			
+			list.head = Novo
+			list.inserted++
+			return nil
+		}
+		var aux  *Node2P
+		if index <= list.inserted/2 {
+		aux = list.head
+		for i := 0; i < index-1; i++ {
+			aux = aux.next
+		}
+	} else {
+		aux = list.tail
+		for i := list.inserted - 1; i > index-1; i-- {
+			aux = aux.previous
+		}
+	}
+
+	}
+	return errors.New(fmt.Sprintf("Indice invalido: %d", index))
 }
