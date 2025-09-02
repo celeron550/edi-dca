6d4d2bf79d16d0d12ba25e23e5a5ff470e4c5362
celeron550 <celeron550@protonmail.com>
2025-09-02 17:07:06 +0000

    modified:   queue/ArrayQueue.go

--- queue/ArrayQueue.go
+++ queue/ArrayQueue.go
@@ -28,6 +28,17 @@ func (queue *ArrayQueue) Size() int{
 	return len(queue.v) - queue.front + queue.rear + 1 // deu a volta
 }
 
+func (queue *ArrayQueue) doubleV() {
+	novoV := make([]int,len(queue.v)*2)
+	novoI := 0
+	for i := queue.front; novoI <len(queue.v); novoI++  {
+		novoV[i] = queue.v[i]
+		i = (i+1)%len(queue.v)
+
+	}
+	queue.v = novoV
+}
+
 func (queue *ArrayQueue) Push(val int) {
 	if queue.front == -1 && queue.rear == -1 { // fila vazia
 		queue.front++
