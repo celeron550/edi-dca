0ec0f395c7a53aba740c2b45485fef074687c25f
Félix Luiz <143456284+celeron550@users.noreply.github.com>
2025-09-04 20:40:23 -0300

    fim da implementação da deque

--- queue/Deque.go
+++ queue/Deque.go
@@ -1,12 +1,14 @@
 package queue
 
-type Node2P struct{
-	val int
+import "errors"
+
+type Node2P struct {
+	val  int
 	prev *Node2P
 	next *Node2P
 }
 
-type Deque struct{
+type Deque struct {
 	head     *Node2P
 	tail     *Node2P
 	inserted int
@@ -16,32 +18,77 @@ func (d *Deque) Size() int {
 	return d.inserted
 }
 
-func (d *Deque) Push_Back(e int){
-
+func (d *Deque) Push_Back(e int) {
+	Novo := &Node2P{
+		val: e,
+	}
+	if d.inserted == 0 {
+		d.head, d.tail = Novo, Novo
+	}
+	Novo.prev = d.tail
+	d.tail = Novo
+	d.inserted++
 }
 
-func (d *Deque) Push_Front(e int){
-
+func (d *Deque) Push_Front(e int) {
+	Novo := &Node2P{
+		val: e,
+	}
+	if d.inserted == 0 {
+		d.head, d.tail = Novo, Novo
+	}
+	Novo.next = d.head
+	d.head = Novo
+	d.inserted++
 }
 
-
-func (d *Deque) Pop_Front(e int){
-
+func (d *Deque) Pop_Front() (int, error) {
+	if d.inserted == 0 {
+		return -1, errors.New("deque vazio")
+	}
+	val := d.head.val
+	if d.inserted == 1{
+		d.head, d.tail = nil,nil
+		d.inserted--
+		return val, nil
+	}
+	d.head = d.head.next
+	d.head.prev = nil
+	d.inserted--
+	return val, nil
 }
 
-func (d *Deque) Pop_Back(e int){
-
+func (d *Deque) Pop_Back() (int, error) {
+	if d.inserted == 0 {
+		return -1, errors.New("deque vazio")
+	}
+	val := d.tail.val
+	if d.inserted == 1{
+		d.head, d.tail = nil,nil
+		d.inserted--
+		return val, nil
+	}
+	d.tail = d.tail.prev
+	d.tail.next = nil
+	d.inserted--
+	return val, nil
 }
 
-func (d *Deque) Peek_Back() (int,error){
-
+func (d *Deque) Peek_Back() (int, error) {
+	if d.inserted == 0 {
+		return -1, errors.New("deque vazio")
+	}
+	return d.tail.val, nil
 }
 
-func (d *Deque) Peek_Front() (int,error){
-
+func (d *Deque) Peek_Front() (int, error) {
+	if d.inserted == 0 {
+		return -1,errors.New("deque vazio")
+	}
+	return d.head.val, nil
 }
 
-func (d *Deque) IsEmpty() bool{
-
+func (d *Deque) IsEmpty() bool {
+	return d.inserted == 0
 }
 
---
+++ search-algorithms/binary-search.go
@@ -0,0 +1,33 @@
+package searchalgorithms
+
+func BinarySearch(v []int, e int, ini int, fim int) int{
+	if ini > fim {return -1}
+
+	mid := (fim+ini)/2
+	if v[mid] == e {
+		return mid
+	} else if v[mid] > e { //ta na primeira metade
+		return BinarySearch(v,e,ini,mid-1) // mid-1 pq ja checou o mid
+
+	}else{ // segunda metade
+		return BinarySearch(v,e,mid+1,fim)
+	}
+	
+}
+
+func BinarySearch_Iterative(v []int, e int) int{
+	ini := 0
+	fim := len(v)-1
+	for fim > ini{
+		mid := (fim+ini)/2
+		if v[mid] == e {
+			return mid
+		} else if v[mid] > e { // ta na primeira metade
+			fim = mid-1
+		} else { // ta na segunda metade
+			ini = mid+1
+		}
+	}
+	return -1
+}
+
