734a3d5fcf6a1c7d04777dc83fd2d61a25051593
celeron550 <celeron550@protonmail.com>
2025-09-18 17:16:24 +0000

    selection sort ta pago

---
+++ sorting-algorithms/selectionsort.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func SelectionSortOP(v []int) []int {
+	ord := make([]int, len(v))
+	MAX := math.MaxInt32
+
+	for i := 0; i < len(v); i++ { // varredura
+		menor := i
+		for j := 0; j < len(v); j++ { // comparar o menor com o resto do vetor
+			if v[j] < v[menor] {
+				menor = j
+			}
+		}
+		ord[i] = v[menor]
+		v[menor] = MAX
+	}
+	return ord
+
+}
+
+func SelectionSortIP(v []int) {
+	for i := 0; i < len(v); i++ { // varredura
+		menor := i
+		for j := 0; j < len(v)-1; j++ { // comparar o menor com o resto do vetor
+			if v[j] < v[menor] {
+				menor = j
+			}
+		}
+		v[i],v[menor] = v[menor],v[i]
+		
+		
+	}
+	
+}
+
+
+func main() {
+	l := []int{9, 15, 7, 235, 8, 1, 2}
+	fmt.Printf("%d", SelectionSortOP(l))
+	l2 := []int{9, 15, 7, 235, 8, 1, 2}
+	fmt.Println(l2)
+	SelectionSortIP(l2)
+	fmt.Println(l2)
+
+}
