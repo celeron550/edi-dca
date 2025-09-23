a8110fdd687e2d78b03737e9ebdeadfa61efc83d
celeron550 <celeron550@protonmail.com>
2025-09-23 16:59:47 +0000

    bubblesortbros..

--- sorting-algorithms/selectionsort.go
+++ sorting-algorithms/selectionsort.go
@@ -9,14 +9,14 @@ func SelectionSortOP(v []int) []int {
 	ord := make([]int, len(v))
 	MAX := math.MaxInt32
 
-	for i := 0; i < len(v); i++ { // varredura
-		menor := i
+	for varredura := 0; varredura < len(v); varredura++ { // varredura
+		menor := varredura
 		for j := 0; j < len(v); j++ { // comparar o menor com o resto do vetor
 			if v[j] < v[menor] {
 				menor = j
 			}
 		}
-		ord[i] = v[menor]
+		ord[varredura] = v[menor]
 		v[menor] = MAX
 	}
 	return ord
@@ -24,20 +24,34 @@ func SelectionSortOP(v []int) []int {
 }
 
 func SelectionSortIP(v []int) {
-	for i := 0; i < len(v)-1; i++ { // varredura
-		menor := i
-		for j := i+1; j < len(v); j++ { // comparar o menor com o resto do vetor
+	for varredura := 0; varredura < len(v)-1; varredura++ { // varredura
+		menor := varredura
+		for j := varredura + 1; j < len(v); j++ { // comparar o menor com o resto do vetor
 			if v[j] < v[menor] {
 				menor = j
 			}
 		}
-		v[i],v[menor] = v[menor],v[i]
-		
-		
+		v[varredura], v[menor] = v[menor], v[varredura]
+
 	}
-	
+
 }
 
+func BubbleSort(v []int) {
+	trocou := false
+	n := len(v)
+	for varredura:=0; varredura<n-1; varredura++ {
+		for j:=0; j<(n-varredura-1); j++{ // sempre vai diminuindo em relação a varredura, para evitar comparacao desnecessaria
+			if v[j] > v[j+1] {
+				v[j], v[j+1] = v[j+1],v[j]
+				trocou = true
+			}else{
+				trocou = false
+			}
+		}
+		if !trocou{break}
+	}
+}
 
 func main() {
 	l := []int{9, 15, 7, 235, 8, 1, 2}
@@ -46,5 +60,9 @@ func main() {
 	fmt.Println()
 	SelectionSortIP(l2)
 	fmt.Println(l2)
+
+	l3 := []int{2, 8, 6, 10, 4, 5, 3}
+	BubbleSort(l3)
+	fmt.Println(l3)
 
 }
