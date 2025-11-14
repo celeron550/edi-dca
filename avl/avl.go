4159aa347400aee200f33313f16acd9c5c376ebe
celeron550 <celeron550@protonmail.com>
2025-11-14 22:44:41 +0000

    rebalance feito

--- avl/avl.go
+++ avl/avl.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	
 )
 
-
 type BstNode struct {
-    left *BstNode
-    value int
-    right *BstNode
-    height int
-    bf int
+	left   *BstNode
+	value  int
+	right  *BstNode
+	height int
+	bf     int
 }
 
-type AVL interface{
+type AVL interface {
 	add(int)
 	search(int) bool
 	min() (int, error)
@@ -28,25 +26,25 @@ type AVL interface{
 }
 
 func (root *BstNode) RotRight() *BstNode {
-    newRoot := root.left
-    root.left = newRoot.right
-    newRoot.right = root
-	
+	newRoot := root.left
+	root.left = newRoot.right
+	newRoot.right = root
+
 	root.UpdateProperties()
 	newRoot.UpdateProperties()
-    
+
 	return newRoot
 
 }
 
 func (root *BstNode) RotLeft() *BstNode {
 	newRoot := root.right
-	root.right = root.left
-	newRoot.left = root	
+	root.right = newRoot.left
+	newRoot.left = root
 	// ordem importa
 	root.UpdateProperties()
 	newRoot.UpdateProperties()
-	
+
 	return newRoot
 }
 
@@ -60,53 +58,59 @@ func (root *BstNode) UpdateProperties() {
 		hr = root.right.height
 	}
 	root.bf = hr - hl
-	if root.left == nil && root.right == nil { // noh eh folha 
+	if root.left == nil && root.right == nil { // noh eh folha
 		root.height = 0
-	}else if hl > hr {
-        root.height = hl + 1
-    } else {
-        root.height = hr + 1
-    }
-
+	} else if hl > hr {
+		root.height = hl + 1
+	} else {
+		root.height = hr + 1
+	}
 
 }
 
-func (root *BstNode) RebalanceLeftLeft() *BstNode{
+func (root *BstNode) RebalanceLeftLeft() *BstNode {
 	return root.RotRight()
 }
 
-func (root *BstNode) RebalanceLeftNeutral() *BstNode{
+func (root *BstNode) RebalanceLeftNeutral() *BstNode {
 	return root.RotRight()
 }
 
-func (root *BstNode) RebalanceLeftRight() *BstNode{
+func (root *BstNode) RebalanceLeftRight() *BstNode {
 	root.left = root.left.RotLeft()
 	return root.RotRight()
 }
 
-func (root *BstNode) RebalanceRightRight() *BstNode{
+func (root *BstNode) RebalanceRightRight() *BstNode {
 	return root.RotLeft()
 }
 
-func (root *BstNode) RebalanceRightNeutral() *BstNode{
+func (root *BstNode) RebalanceRightNeutral() *BstNode {
 	return root.RotLeft()
 }
 
-func (root *BstNode) RebalanceRightLeft() *BstNode{
+func (root *BstNode) RebalanceRightLeft() *BstNode {
 	root.right = root.right.RotRight()
 	return root.RotLeft()
 
 }
 
-func (root *BstNode) Rebalance() *BstNode{
+func (root *BstNode) Rebalance() *BstNode {
 	// checa o fator de balanço e rebalanceia
-	if root.bf < 0 { // ela ta torta pra esquerda
-		if root.left.bf < 0 { // caso left-left e left-neutral
+	if root.bf < -1 { // torta para a esquerda
+		if root.left.bf <= 0 { // casos Left-Left e Left-Neutral
 			return root.RebalanceLeftLeft()
-		}else{ // caso left-right
+		} else { // caso Left-Right
 			return root.RebalanceLeftRight()
 		}
+	} else if root.bf > 1 { // torta para a direita
+		if root.right.bf >= 0 { // casos Right-Right e Right-Neutral
+			return root.RebalanceRightRight()
+		} else { // caso Right-Left
+			return root.RebalanceRightLeft()
+		}
 	}
+	return root // ja ta balanceada
 }
 
 func main() {
